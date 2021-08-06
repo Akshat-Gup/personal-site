f58637bf5169b103ab6c494d74e21d131985bbd7
Akshat Gupta <akshatguptabusiness@gmail.com>
2021-08-06 21:51:59 +0800

    redesigning site?

---
+++ aboutme.js
@@ -0,0 +1,16 @@
+let code = document.querySelector('.code');
+let music = document.querySelector('.music');
+let chess = document.querySelector('.chess');
+let eightball = document.querySelector('.eightball');
+music.addEventListener('click', ()=> {
+	code.classList.remove('height');
+	code.innerHTML = "<a href='https://practinstrument.herokuapp.com/'><img src='guitar-app.png'/></a>";
+})
+chess.addEventListener('click', ()=> {
+	code.classList.remove('height');
+	code.innerHTML = "<a href='https://akshat-gup.github.io/Chess-Board/'><img src='chessboard.png'/></a>";
+})
+eightball.addEventListener('click', ()=> {
+	code.classList.remove('height');
+	code.innerHTML = "<a href='https://akshat-gup.github.io/8-Ball/'><img src='8-ball.png'/></a>";
+})
